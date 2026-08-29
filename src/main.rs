2ee1551548b4ed5f0e2d6324e000a6d2a0538ad5
Caleb Owens <caleb@gitbutler.com>
2026-08-29 13:39:16 +0200

    Hear what the client has to say
    Listening on localhost:3000
    Talking to: 127.0.0.1:54270
    GET / HTTP/1.1
    Host: localhost:3000
    User-Agent: curl/8.7.1
    Accept: */*

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::{BufReader, BufRead as _};
 
 fn main() -> std::io::Result<()> {
     let address = "localhost:3000";
@@ -10,6 +11,17 @@ fn main() -> std::io::Result<()> {
 
         let connector = stream.peer_addr()?;
         println!("Talking to: {}", connector);
+
+        let mut reader = BufReader::new(stream.try_clone()?);
+
+        let mut line = String::new();
+        loop {
+            reader.read_line(&mut line)?;
+            print!("{line}");
+            line.clear();
+
+            // TODO: We should probably stop at some point...
+        }
     }
 
     Ok(())
