197066388a86b91bc182e6a838f052d3897cf1fb
Caleb Owens <caleb@gitbutler.com>
2026-08-29 18:44:38 +0200

    Implement debug printing for ParseResults
    `We got: ParseResult::Found { subject: [104, 101, 108, 108, 111], rest: " world" }`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use std::io::{BufRead as _, BufReader};
 use std::net::TcpListener;
 
@@ -31,11 +32,32 @@ fn tcp() -> std::io::Result<()> {
     Ok(())
 }
 
-
 #[derive(PartialEq, Eq)]
 enum ParseResult<'a, T> {
     Found { subject: T, rest: &'a [u8] },
     Missed { rest: &'a [u8] },
+}
+
+impl<'a, T: Debug> Debug for ParseResult<'a, T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Found { subject, rest } => f
+                .debug_struct("ParseResult::Found")
+                .field("subject", subject)
+                .field(
+                    "rest",
+                    &std::str::from_utf8(rest).unwrap_or("non-utf8 bytes"),
+                )
+                .finish(),
+            Self::Missed { rest } => f
+                .debug_struct("ParseResult::Missed")
+                .field(
+                    "rest",
+                    &std::str::from_utf8(rest).unwrap_or("non-utf8 bytes"),
+                )
+                .finish(),
+        }
+    }
 }
 
 fn term_p<'i, 't>(input: &'i [u8], term: &'t [u8]) -> ParseResult<'i, &'t [u8]> {
@@ -49,11 +71,12 @@ fn term_p<'i, 't>(input: &'i [u8], term: &'t [u8]) -> ParseResult<'i, &'t [u8]> 
     }
 }
 
-
 fn main() -> std::io::Result<()> {
     // tcp()?;
 
     let outcome = term_p(b"hello world", b"hello");
+
+    println!("We got: {:?}", outcome);
 
     Ok(())
 }
