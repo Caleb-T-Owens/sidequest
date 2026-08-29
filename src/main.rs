bd4cda30f91a308c8d1d8ed2f0a52172a542feca
Caleb Owens <caleb@gitbutler.com>
2026-08-29 13:13:48 +0200

    Add TCP listener
    Logging whenever my browser touches the client:
    
    ```
    Listening on localhost:3000
    A stream has been found!
    A stream has been found!
    A stream has been found!
    A stream has been found!
    A stream has been found!
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+fn main() -> std::io::Result<()> {
+    let address = "localhost:3000";
+    let listener = TcpListener::bind(address)?;
+    println!("Listening on {address}");
+
+    for _stream in listener.incoming() {
+        println!("A stream has been found!");
+    }
+
+    Ok(())
 }
